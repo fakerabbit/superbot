6a79702e01abc364c3a83c3c5d43fcb42e1eaf01
Mirko Justiniano <mirkoj@gmail.com>
2018-05-24 20:31:43 -0500

    Added parameter meme

--- app.js
+++ app.js
@@ -355,8 +355,8 @@ function sendToBot(senderID, currentUser, message) {
           const parameters = result.parameters;
           console.log('action: ', action);
           console.log('parameters: ', parameters);
-          if (parameters) {
-            getMeme(senderID, parameters);
+          if (parameters && parameters.meme) {
+            getMeme(senderID, parameters.meme);
           }
           if (action) {
             switch (action) {
